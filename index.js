a9ac74166a28cb43bd281622d20ba1e2eebed7f9
tech-gunktest <94447223+tech-gunktest@users.noreply.github.com>
2021-11-18 15:17:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 aprocess = require('child_process');
 fs = require('fs');
-shell = require('shelljs')
 
 app.use(express.urlencoded({ extended: false }))// server to understand what app data is
 app.use(express.json())
@@ -49,21 +48,36 @@ app.post('/compile/node', (req, res)=>{
     })
 });
 
-app.post('/compile/cpp', (req, res)=>{
-    var k = getRandomString(16)
-    var f= k+'.cpp'
+app.post('/compile/ruby', (req, res)=>{
+    var f= getRandomString(16)+'.rb'
     if(fs.existsSync(f)){
-        f= getRandomString(8)+'.cpp'
+        f= getRandomString(8)+'.rb'
     }else{
     fs.writeFile(f, req.body.code, (err)=>{
         if(err) throw err;
     })
 }
-    shell.exec('make '+k+' && ./'+k, function(code, stdout, stderr) {
-  console.log('Exit code:', code);
-  res.send('Program output:', stdout);
-  console.log('Program stderr:', stderr);
+    const ruby = aprocess.spawn('ruby', [f])
+    ruby.stdout.on('data', (data)=>{
+        res.send(data.toString())
+    })
 });
-    
+
+app.post('/compile/perl', (req, res)=>{
+    var f= getRandomString(16)+'.pl'
+    if(fs.existsSync(f)){
+        f= getRandomString(8)+'.pl'
+    }else{
+    fs.writeFile(f, req.body.code, (err)=>{
+        if(err) throw err;
+    })
+}
+    const perl = aprocess.spawn('perl', [f])
+    perl.stdout.on('data', (data)=>{
+        res.send(data.toString())
+    })
+});
+
+app.listen(process.env.port||4000, function(){
+    console.log('listening on http://localhost:4000')
 })
-app.listen(process.env.PORT || 3000);
